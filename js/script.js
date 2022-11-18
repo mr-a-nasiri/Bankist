b81db339a6edd87fe1f6484ff3e9d5031c37adc9
mr-a-nasiri <mrfinal21@gmail.com>
2022-11-18 16:51:37 +0330

    Add Revealing Sections on Scroll Effect

--- js/script.js
+++ js/script.js
@@ -47,23 +47,55 @@ document.querySelector('.operations__tab-container').addEventListener('click', f
 /////----------- Sticky Navigation -----------/////
 ///////////////////////////////////////////////////////
 
-const nav = document.querySelector('.nav');
-const observer = new IntersectionObserver(
-  entries => {
-    // entry is threshold
-    const [entry] = entries;
+const stickyNav = function () {
+  const nav = document.querySelector('.nav');
+  const observer = new IntersectionObserver(
+    entries => {
+      // entry is threshold
+      const [entry] = entries;
 
-    if (!entry.isIntersecting) nav.classList.add('sticky');
-    else nav.classList.remove('sticky');
-  },
-  {
-    // root null is viewport
-    root: null,
-    // threshold is percent of observing target in the root
-    threshold: 0,
-    rootMargin: `${-nav.getBoundingClientRect().height}px`,
-  }
-);
+      if (!entry.isIntersecting) nav.classList.add('sticky');
+      else nav.classList.remove('sticky');
+    },
+    {
+      // root null is viewport
+      root: null,
+      // threshold is percent of observing target in the root
+      threshold: 0,
+      rootMargin: `${-nav.getBoundingClientRect().height}px`,
+    }
+  );
 
-const header = document.querySelector('.header');
-observer.observe(header);
+  const header = document.querySelector('.header');
+  observer.observe(header);
+};
+stickyNav();
+
+//////////////////////////////////////////////////////////////////////
+/////----------- Revealing Sections on Scroll -----------/////
+//////////////////////////////////////////////////////////////////////
+
+const revealSection = function () {
+  const observer = new IntersectionObserver(
+    entries => {
+      // entry is threshold
+      const [entry] = entries;
+
+      if (!entry.isIntersecting) return;
+
+      // entry.target is the intersecting element
+      entry.target.classList.toggle('section--hidden');
+      observer.unobserve(entry.target);
+    },
+    {
+      // root null is viewport
+      root: null,
+      // threshold is percent of observing target in the root
+      threshold: 0.2,
+    }
+  );
+
+  const sections = document.querySelectorAll('.section');
+  sections.forEach(section => observer.observe(section));
+};
+revealSection();
