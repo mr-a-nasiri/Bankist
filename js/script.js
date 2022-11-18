bdd28096e794304522e6d48d70dccc966f492902
mr-a-nasiri <mrfinal21@gmail.com>
2022-11-18 14:22:14 +0330

    Building Tabbed Component

--- js/script.js
+++ js/script.js
@@ -16,5 +16,29 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 ///////////////////////////////////////////////////////
-/////----------- Smooth Scrolling -----------/////
+/////----------- Tabbed Component -----------/////
 ///////////////////////////////////////////////////////
+
+document.querySelector('.operations__tab-container').addEventListener('click', function (e) {
+  if (!e.target.closest('.operations__tab')) return;
+
+  const targetTab = e.target.closest('.operations__tab').dataset.tab;
+
+  // Remove active class from all content
+  document.querySelectorAll('.operations__content').forEach(function (tab) {
+    tab.classList.remove('operations__content--active');
+  });
+
+  // Remove active class from all tabs
+  document.querySelectorAll('.operations__tab').forEach(function (tab) {
+    tab.classList.remove('operations__tab--active');
+  });
+
+  // Add active class to target content
+  document
+    .querySelector(`.operations__content--${targetTab}`)
+    .classList.add('operations__content--active');
+
+  // Add active class to target tab
+  document.querySelector(`.operations__tab--${targetTab}`).classList.add('operations__tab--active');
+});
