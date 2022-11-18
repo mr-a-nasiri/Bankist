e6f5d5c10c314be92ff90d7fde609561f9e11d53
mr-a-nasiri <mrfinal21@gmail.com>
2022-11-18 17:36:43 +0330

    Reformatted the codes

---
+++ js/UI.js
@@ -0,0 +1,125 @@
+export class UI {
+  ///////////////////////////////////////////////////////
+  /////----------- Smooth Scrolling -----------/////
+  ///////////////////////////////////////////////////////
+
+  static smoothScrolling() {
+    document.querySelector('.nav__links').addEventListener('click', function (e) {
+      if (!e.target.classList.contains('nav__link')) return;
+
+      const id = e.target.getAttribute('href');
+      const target = document.querySelector(id);
+
+      target.scrollIntoView({ behavior: 'smooth' });
+
+      e.preventDefault();
+    });
+  }
+
+  ///////////////////////////////////////////////////////
+  /////----------- Tabbed Component -----------/////
+  ///////////////////////////////////////////////////////
+
+  static tabbedComponent() {
+    document.querySelector('.operations__tab-container').addEventListener('click', function (e) {
+      if (!e.target.closest('.operations__tab')) return;
+
+      const targetTab = e.target.closest('.operations__tab').dataset.tab;
+
+      // Remove active class from all content
+      document.querySelectorAll('.operations__content').forEach(function (tab) {
+        tab.classList.remove('operations__content--active');
+      });
+
+      // Remove active class from all tabs
+      document.querySelectorAll('.operations__tab').forEach(function (tab) {
+        tab.classList.remove('operations__tab--active');
+      });
+
+      // Add active class to target content
+      document
+        .querySelector(`.operations__content--${targetTab}`)
+        .classList.add('operations__content--active');
+
+      // Add active class to target tab
+      document
+        .querySelector(`.operations__tab--${targetTab}`)
+        .classList.add('operations__tab--active');
+    });
+  }
+
+  ///////////////////////////////////////////////////////
+  /////----------- Sticky Navigation -----------/////
+  ///////////////////////////////////////////////////////
+
+  static stickyNav() {
+    const nav = document.querySelector('.nav');
+    const observer = new IntersectionObserver(
+      entries => {
+        const [entry] = entries;
+
+        if (!entry.isIntersecting) nav.classList.add('sticky');
+        else nav.classList.remove('sticky');
+      },
+      {
+        root: null,
+        threshold: 0,
+        rootMargin: `${-nav.getBoundingClientRect().height}px`,
+      }
+    );
+
+    const header = document.querySelector('.header');
+    observer.observe(header);
+  }
+
+  //////////////////////////////////////////////////////////////////////
+  /////----------- Revealing Sections on Scroll -----------/////
+  //////////////////////////////////////////////////////////////////////
+
+  static revealSection() {
+    const observer = new IntersectionObserver(
+      entries => {
+        const [entry] = entries;
+
+        if (!entry.isIntersecting) return;
+
+        entry.target.classList.toggle('section--hidden');
+        observer.unobserve(entry.target);
+      },
+      {
+        root: null,
+        threshold: 0.2,
+      }
+    );
+
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => observer.observe(section));
+  }
+
+  ///////////////////////////////////////////////////
+  /////----------- Lazy Loading -----------/////
+  ///////////////////////////////////////////////////
+
+  static lazeLoading() {
+    const observer = new IntersectionObserver(
+      entries => {
+        const [entry] = entries;
+
+        if (!entry.isIntersecting) return;
+
+        entry.target.src = entry.target.dataset.src;
+
+        entry.target.addEventListener('load', () => entry.target.classList.remove('lazy-img'));
+        observer.unobserve(entry.target);
+      },
+      {
+        root: null,
+        threshold: 0,
+        rootMargin: '200px',
+      }
+    );
+
+    const LazyImages = document.querySelectorAll('.lazy-img');
+    LazyImages.forEach(Image => observer.observe(Image));
+  }
+}
---
+++ js/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import { UI } from '/js/UI.js';
+
+UI.smoothScrolling();
+UI.tabbedComponent();
+UI.stickyNav();
+UI.revealSection();
+UI.lazeLoading();
